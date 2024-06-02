cd4f95083eabf256052fe3a13236884efed9af82
munist1010 <aaronjkellywork@gmail.com>
2024-06-02 22:56:35 +0100

    fresh merge

---
+++ src/main/java/com/munist/project/java/springboot/project/JavaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.munist.project.java.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munist/project/java/springboot/project/JavaSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munist.project.java.springboot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
